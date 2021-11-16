6b1cbee9d8c1e5b673688f41416a434802d7d970
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-11-16 22:59:15 +0300

    немного обновок

--- src/main/java/com/securityHW/config/AppSecurityConfig.java
+++ src/main/java/com/securityHW/config/AppSecurityConfig.java
@@ -3,6 +3,7 @@ package com.securityHW.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.securityHW.config.ApplicationUserPermission.TASK_WRITE;
 import static com.securityHW.config.ApplicationUserRole.*;
 
 @RequiredArgsConstructor
@@ -24,9 +26,12 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","index").permitAll()  //позволит открывать эти странички всем
-                .antMatchers("/manager/api/**").hasAnyRole(MANAGER.name(),SCRUM_MASTER.name())
+                .antMatchers("/manager/api/**").hasAnyRole(MANAGER.name(),SCRUM_MASTER.name()) //доступно только для манагера и скрама
+                .antMatchers(HttpMethod.PUT,"api/task").hasAuthority(TASK_WRITE.getPermission()) //закрываем доступ к api/task.Можно указывать не только Паттер эдпоинта("api/task"), но и метода.
+                //Резюмирую,то есть,чтобы вызывать этот метод Пут,нужно иметь права доступа к ТаскВрайт
                 .anyRequest()
                 .authenticated()
                 .and()
