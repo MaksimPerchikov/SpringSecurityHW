43c88a9487265ad8d69c92cd9d057a353f2d9a00
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-11-17 07:04:12 +0300

    исправления какие-то там по счету

--- src/main/java/com/securityHW/controller/UpdateToken.java
+++ src/main/java/com/securityHW/controller/UpdateToken.java
@@ -1,34 +1,25 @@
+
 package com.securityHW.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/update/token")
-public class UpdateToken extends UsernamePasswordAuthenticationFilter {
+public class UpdateToken{
 
     @PreAuthorize("hasRole('SCRUM_MASTER')")
     @GetMapping
-    public void updateToken(){/*(HttpServletResponse response, Authentication authResult) throws IOException{
-        JwtProvider jwtProvider = new JwtProvider();
-        ObjectMapper objectMapper = new ObjectMapper();
+    public void updateToken(){
 
-        String tokenFirst = jwtProvider.createToken(authResult);
-        String tokenRefresh = jwtProvider.createRefreshToken(authResult);
 
-        Map<String, String> mapToken = new HashMap<>();
-        mapToken.put("first token", tokenFirst);
-        mapToken.put("first refresh", tokenRefresh);
 
-        response.setContentType("application/json");
-
-        objectMapper.writeValue(response.getOutputStream(), mapToken);*/
 
 
     }
 
 
 }
+
